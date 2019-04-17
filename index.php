6779e4d56716b1fb48a79f9135be49ceb8f64db5
GiedreJ <saulevakarine1@gmail.com>
2019-04-17 16:22:17 +0300

    update

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
-
+<?php
+    $img = "IMG/bomb.png";
+            ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title></title>
         <meta charset="UTF-8">
     </head>
-    <body style="background-color: rgb(<?php print rand(000, 255) . ', ' . rand(000, 255) . ', ' . rand(000, 255);?>)">
-        <h1 style="font-size: <?php print rand(12, 140);?>px">
-            Aš keičiu savo dydį!
-        </h1>
-        <p style="color: rgb(<?php print rand(000, 255) . ', ' . rand(000, 255) . ', ' . rand(000, 255);?>)">
-            Aš keičiu savo spalvą!
-        </p>
-        <img src="IMG/<?php print rand(1, 6); ?>.png">
-        
-        <div>
-            <img src="IMG/bomb.png" style="width:<?php print rand(10, 30);?>%">
-        </div>
-        <div>
-            <?php print date('s');?>
-        </div>
-        <p>
-            testas
-        </p>
-        
-        
-        
+    <body>
+        <img src="<?php print $img ?>">
+        <img src="<?php print $img ?>">
+        <img src="<?php print $img ?>">
+        <img src="<?php print $img ?>">
     </body>
 </html>
 
