e47460ae5025cd70cd2946e1fb0b5f5b6e529c89
GiedreJ <saulevakarine1@gmail.com>
2019-04-17 16:49:42 +0300

    update

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
-    $img = "IMG/bomb.png";
-            ?>
+
+    $skola = rand(1,5);
+    $_2metai = rand(8,9);
+    $_1metai = rand(6,7);
+    
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title>Skolos skaičiuoklė</title>
         <meta charset="UTF-8">
     </head>
     <body>
-        <img src="<?php print $img ?>">
-        <img src="<?php print $img ?>">
-        <img src="<?php print $img ?>">
-        <img src="<?php print $img ?>">
+        <div class="body1">
+            <h1>Skolos skaičiuoklė</h1>
+            <h3>Jei paėmei <?php print $skola;?> Eur</h3>
+            <h3>Už 1 metų grąžinsi <?php print $_1metai?></h3>
+            <h3>Už 2 metų grąžinsi <?php print $_2metai;?></h3>    
+        </div>
     </body>
 </html>
 
