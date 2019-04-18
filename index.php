aa108877e18a78b7ae454365762552699e187c23
GiedreJ <saulevakarine1@gmail.com>
2019-04-18 11:07:24 +0300

    update

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
-<?php
-
-    $skola = rand(1,5);
-    $_2metai = rand(8,9);
-    $_1metai = rand(6,7);
-    
-?>
 <!DOCTYPE html>
+
 <html>
     <head>
-        <title>Skolos skaičiuoklė</title>
+        <title>Šiukšlės</title>
         <meta charset="UTF-8">
     </head>
     <body>
-        <div class="body1">
-            <h1>Skolos skaičiuoklė</h1>
-            <h3>Jei paėmei <?php print $skola;?> Eur</h3>
-            <h3>Už 1 metų grąžinsi <?php print $_1metai?></h3>
-            <h3>Už 2 metų grąžinsi <?php print $_2metai;?></h3>    
-        </div>
+	<?php 
+
+	$siukslines_turis = 40;
+	$siuksliu_turis_avg_per_day = 15;
+	$max_kaupo_turis = rand(1,10);
+	$dienu = floor(($siukslines_turis + $max_kaupo_turis) / $siuksliu_turis_avg_per_day);
+	print $dienu . ' ' . date('Y-m-d', strtotime("+$dienu days"));
+
+	?>
     </body>
 </html>
-
-
-
