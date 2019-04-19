05c7096944e64b96014007c739270e54df25e0b6
GiedreJ <saulevakarine1@gmail.com>
2019-04-19 11:09:40 +0300

    sauleta/debesuota/lietinga/vaivorykste_ikonos

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ $sunny = rand(0, 1);
 $rainy = rand (0, 1);
 
 if ($sunny && $rainy) {
-    $css_class = 'rainbow';
+    $css_class = 'fas fa-rainbow';
     $tekstas = "Vaivorykštė";
 }   elseif ($sunny XOR $rainy) {
         if ($sunny) {
-        $css_class = 'sunny';
+        $css_class = 'fas fa-sun';
         $tekstas = "Saulėta";
       } elseif ($rainy) {
-        $css_class = 'rainy';
+        $css_class = 'fas fa-cloud-rain';
         $tekstas = "Lietus";
       }
 }   else {
-    $css_class = 'cloudy';
+    $css_class = 'fas fa-cloud';
     $tekstas = "Debesuota";
 } 
 
@@ -29,33 +29,7 @@ if ($sunny && $rainy) {
         <title></title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-        <style type="text/css">
-            .sunny {
-                background-image: url(IMG/sunny.png);
-                width: 150px;
-                height: 150px;
-                background-size: cover;
-            }
-            .cloudy {
-                background-image: url(IMG/cloudy.png);
-                width: 150px;
-                height: 150px;
-                background-size: cover;
-            }
-            .rainbow {
-                background-image: url(IMG/rainbow.png);
-                width: 150px;
-                height: 150px;
-                background-size: cover;
-            }
-            .rainy {
-                background-image: url(IMG/rainy.png);
-                width: 150px;
-                height: 150px;
-                background-size: cover;
-            }
-            
-        </style>
+        <style></style>
     </head>
     <body>
         <div class ="container">
